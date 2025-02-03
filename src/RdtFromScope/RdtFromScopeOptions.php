756086a1ac2e70e6842063b5bceb84b12ebee318
david_smith <david_smith@sweetwater.com>
2025-02-02 19:18:08 -0500

    feat/cli Created `src/RdtFromScope/RdtFromScopeOptions.php`.

---
+++ src/RdtFromScope/RdtFromScopeOptions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zerotoprod\SpapiTokensCli\RdtFromScope;
+
+use Zerotoprod\DataModel\DataModel;
+
+class RdtFromScopeOptions
+{
+    use DataModel;
+
+    public const user_agent = 'user_agent';
+    public ?string $user_agent = null;
+
+    public const response = 'response';
+    public bool $response = false;
+
+    public const expiresIn = 'expiresIn';
+    public bool $expiresIn = false;
+}
